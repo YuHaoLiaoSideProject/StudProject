c45df95eaf0370d48389e6c017109e775dc30f16
yuhaoliao <h770320@gmail.com>
2021-05-18 16:58:45 +0800

    ２對

---
+++ StudProject/CardVerifies/TwoPairsCardVerify.cs
@@ -0,0 +1,18 @@
+﻿using StudProject.Models;
+
+namespace StudProject.CardVerifies
+{
+    public class TwoPairsCardVerify : BaseCardVerify
+    {
+        public TwoPairsCardVerify(VerifyModel verify) : base(verify) { }
+
+        public override bool IsValidate()
+        {
+            //篩選數量為２的資料且兩組
+            return _CardCount.Values.IsContainTimes(quantity: 2, frequency: 2);
+        }
+    }
+
+
+
+}
