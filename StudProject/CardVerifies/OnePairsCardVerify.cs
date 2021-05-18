e31050d29c9be085ea2b52e4237b1279b404ff0b
yuhaoliao <h770320@gmail.com>
2021-05-18 16:58:26 +0800

    對子

---
+++ StudProject/CardVerifies/OnePairsCardVerify.cs
@@ -0,0 +1,15 @@
+﻿using StudProject.Models;
+
+namespace StudProject.CardVerifies
+{
+    public class OnePairsCardVerify : BaseCardVerify
+    {
+        public OnePairsCardVerify(VerifyModel verify) : base(verify) { }
+
+        public override bool IsValidate()
+        {
+            return _CardCount.Values.IsContainTimes(quantity: 2, frequency: 1)
+                && _CardCount.Values.IsContainTimes(quantity: 1, frequency: 3);
+        }
+    }
+}
---
+++ StudProject/ExtIEnumerable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudProject
+{
+    public static class ExtIEnumerable
+    {
+        public static bool IsContainTimes<T>(this IEnumerable<T> array, T quantity, int frequency) where T : IComparable
+        {
+            return array.Count(e => e.Equals(quantity)) == frequency;
+        }
+    }
+
+
+
+}
