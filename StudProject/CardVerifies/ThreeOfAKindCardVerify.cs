bb280a174b46e4dfcbe27f447114412ac39ca1ce
yuhaoliao <h770320@gmail.com>
2021-05-18 17:15:13 +0800

    三條

---
+++ StudProject/CardVerifies/ThreeOfAKindCardVerify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using StudProject.Models;
+
+namespace StudProject.CardVerifies
+{
+    public class ThreeOfAKindCardVerify : BaseCardVerify
+    {
+        public ThreeOfAKindCardVerify(VerifyModel verify) : base(verify) { }
+
+        public override bool IsValidate()
+        {
+            return _CardCount.Values.Contains(3) && _CardCount.Values.IsContainTimes(quantity: 1, frequency: 2);
+        }
+    }
+}
---
+++ StudProjectTest/ThreeOfAKindCardVerifyTest.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudProject;
+using StudProject.CardVerifies;
+using StudProject.Models;
+
+namespace StudProjectTest
+{
+    [TestClass]
+    public class ThreeOfAKindCardVerifyTest
+    {
+        [TestMethod]
+        public void 三條()
+        {
+            List<CardModel> cards = new List<CardModel>
+            {
+                new CardModel{ Type = CardTypeEnum.Club,Value = "3"},
+                new CardModel{ Type = CardTypeEnum.Diamond,Value = "2"},
+                new CardModel{ Type = CardTypeEnum.Heart,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Spade,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Club,Value = "A"},
+            };
+            VerifyModel verify = cards.GetVerifyModel();
+            bool isValidate = new ThreeOfAKindCardVerify(verify).IsValidate();
+            Assert.IsTrue(isValidate);
+        }
+        [TestMethod]
+        public void 不是三條是鐵支()
+        {
+            List<CardModel> cards = new List<CardModel>
+            {
+                new CardModel{ Type = CardTypeEnum.Club,Value = "2"},
+                new CardModel{ Type = CardTypeEnum.Diamond,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Heart,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Spade,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Club,Value = "A"},
+            };
+            VerifyModel verify = cards.GetVerifyModel();
+            bool isValidate = new ThreeOfAKindCardVerify(verify).IsValidate();
+            Assert.IsFalse(isValidate);
+        }
+
+        [TestMethod]
+        public void 不是三條是葫蘆()
+        {
+            List<CardModel> cards = new List<CardModel>
+            {
+                new CardModel{ Type = CardTypeEnum.Club,Value = "2"},
+                new CardModel{ Type = CardTypeEnum.Diamond,Value = "2"},
+                new CardModel{ Type = CardTypeEnum.Heart,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Spade,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Club,Value = "A"},
+            };
+            VerifyModel verify = cards.GetVerifyModel();
+            bool isValidate = new ThreeOfAKindCardVerify(verify).IsValidate();
+            Assert.IsFalse(isValidate);
+        }
+
+        [TestMethod]
+        public void 不是三條是對子()
+        {
+            List<CardModel> cards = new List<CardModel>
+            {
+                new CardModel{ Type = CardTypeEnum.Club,Value = "2"},
+                new CardModel{ Type = CardTypeEnum.Diamond,Value = "3"},
+                new CardModel{ Type = CardTypeEnum.Heart,Value = "4"},
+                new CardModel{ Type = CardTypeEnum.Spade,Value = "A"},
+                new CardModel{ Type = CardTypeEnum.Club,Value = "A"},
+            };
+            VerifyModel verify = cards.GetVerifyModel();
+            bool isValidate = new ThreeOfAKindCardVerify(verify).IsValidate();
+            Assert.IsFalse(isValidate);
+        }
+    }
+}
